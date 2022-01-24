718e241e62bd0ba865b81bfb725ca705b6280655
Ihor Mazur <i.mazur@wnb.com.ua>
2022-01-25 00:18:18 +0200

    Adding a cache strategy

--- src/api/routes/subscriptions.js
+++ src/api/routes/subscriptions.js
@@ -7,14 +7,22 @@ const SubscriptionController = require('../controllers/SubscriptionController');
 const CreateSubscriptionValidator = require('../validators/CreateSubscriptionValidator');
 const CreateSubscriptionController = require('../controllers/CreateSubscriptionController');
 
+const ForeverCacheDecorator = require('../../decorators/ForeverCacheDecorator');
+const RemoveCacheDecorator = require('../../decorators/RemoveCacheDecorator');
+
 function register(router) {
   router.get(
     '/subscriptions',
     validate(new SubscriptionValidator()),
     async (ctx) => {
-      const controller = new SubscriptionController();
+      const { userId, limit = 20 } = ctx.request.query;
 
-      const data = await controller.handle(ctx.request.query);
+      const controller = new ForeverCacheDecorator(
+        new SubscriptionController(),
+        `users.${userId}.subscriptions`
+      );
+
+      const data = await controller.handle({ userId, limit });
 
       respond(ctx, data);
     }
@@ -24,9 +32,14 @@ function register(router) {
     '/subscriptions',
     validate(new CreateSubscriptionValidator()),
     async (ctx) => {
-      const controller = new CreateSubscriptionController();
+      const { userId, subscriptionId } = ctx.request.body;
 
-      const data = await controller.handle(ctx.request.body);
+      const controller = new RemoveCacheDecorator(
+        new CreateSubscriptionController(),
+        `users.${userId}.subscriptions`
+      );
+
+      const data = await controller.handle({ userId, subscriptionId });
 
       return data ? respond(ctx, data) : respond(ctx, null, 404);
     }
